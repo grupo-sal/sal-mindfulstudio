7bbf991a1964d7c13e666fd3414e6ac6220d9916
grupo-sal <sal.club.espinho@gmail.com>
2026-08-26 15:42:30 +0100

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -410,7 +410,7 @@ function Index() {
           </div>
           <div className="sal-footer-bottom">
             <span>© {new Date().getFullYear()} SAL Mindful Studio. Todos os direitos reservados.</span>
-            <span>Espinho, Portugal</span>
+            <span>Espinho, Portugl</span>
           </div>
         </div>
       </footer>
